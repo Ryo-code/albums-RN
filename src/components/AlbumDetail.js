1b5a159f12d1ffd34195d1bd952831c569363e00
Ryo <kanadajinlee@yahoo.ca>
2017-02-05 11:44:42 -0500

    Styled the button with alignSelf and flex1, etc.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image, image } = album;
@@ -15,8 +16,8 @@ const AlbumDetail = ({ album }) => {
 
   return (
     <Card>
-      <CardSection>
 
+      <CardSection>
         <View style={thumbnailContainerStyle}>
           <Image 
           style={thumbnailStyle}
@@ -28,13 +29,17 @@ const AlbumDetail = ({ album }) => {
           <Text style={headerTextStyle}> {title} </Text> 
           <Text> {artist} </Text>
         </View>
-
       </CardSection>
+
       <CardSection>
         <Image 
           style={imageStyle}
           source={{ uri: image }} 
         />
+      </CardSection>
+      
+      <CardSection>
+        <Button/>
       </CardSection>
     </Card>
   );
